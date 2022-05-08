1657d21d24d873c979571a2a5ccc109dcd7a9702
Loth Corea <loth.zrkt@gmail.com>
2022-05-08 17:08:09 -0500

    repaso

--- PI-Videogames-main/api/src/routes/index.js
+++ PI-Videogames-main/api/src/routes/index.js
@@ -1,12 +1,23 @@
 const { Router } = require('express');
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
+const loginrt = require("./login")
+const homert = require("./home")
+const gamedetrt = require("./gamedetails")
+const creatert = require("./creategame")
 
 
 const router = Router();
 
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
+
+router.use("/", loginrt)
+
+router.use("/", homert)
+
+router.use("/home/", gamedetrt)
+
 
 
 module.exports = router;
